4596c95592a7ad4b1d1491a7dfc198bf06155f65
AymaneAbidane <76812961+AymaneAbidane@users.noreply.github.com>
2025-01-21 20:50:01 +0100

    Testing enemys movment by using navmesh navigation package

---
+++ Assets/Game/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float turiningSpeed = 10f;
+
+    [SerializeField] private Transform[] waypointsArray;
+    [SerializeField] private NavMeshAgent ownAgent;
+
+    private int wayPointIndex;
+
+    private void Awake()
+    {
+        ownAgent.updateRotation = false;
+    }
+
+    private void Update()
+    {
+        FaceTarget(ownAgent.steeringTarget);
+        if (ownAgent.remainingDistance < 0.5f)
+        {
+            if (wayPointIndex < waypointsArray.Length)
+            {
+                ownAgent.SetDestination(GetNextWayPoint());
+            }
+        }
+    }
+
+    private void FaceTarget(Vector3 target)
+    {
+        Vector3 directionToTarget = target - transform.position;
+        directionToTarget.y = 0;//ingonre y axis in rotation
+        Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turiningSpeed * Time.deltaTime);
+    }
+
+    private Vector3 GetNextWayPoint()
+    {
+        if (wayPointIndex >= waypointsArray.Length)
+        {
+            return transform.position;
+        }
+        else
+        {
+            Vector3 targetPosition = waypointsArray[wayPointIndex].position;
+            wayPointIndex++;
+            return targetPosition;
+        }
+    }
+}
